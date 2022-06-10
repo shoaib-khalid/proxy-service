af407e6904e89a9cae48ce097b6872e2d5b3de77
nur.iman <nur.iman@kalsym.com>
2022-06-10 18:00:23 +0800

    use device-detector

--- src/main/java/com/kalsym/proxyservice/controller/ProxyController.java
+++ src/main/java/com/kalsym/proxyservice/controller/ProxyController.java
@@ -48,39 +48,17 @@ public class ProxyController {
         try {
 
             // =======================
-            //
+            // deevvicom/device-detector https://github.com/deevvicom/device-detector
             // =======================
-            System.out.println("cHECKINGGGGGGG request.getServerName() host domain:::::"+request.getServerName());//merchant.symplified.test
-            System.out.println("cHECKINGGGGGGG request.getServerPort() :::::"+request.getServerPort());//8080
-            System.out.println("cHECKINGGGGGGG request.getHeader(host):::::"+request.getHeader("host"));//merchant.symplified.test:8080
-            System.out.println("cHECKINGGGGGGG request.getScheme():::::"+request.getScheme());//http
+            String userAgent = request.getHeader("User-Agent");
 
-            String browserName = request.getHeader("User-Agent");
-            System.out.println("cHECKINGGGGGGG browserName:::::"+browserName);//Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33   
+            DeviceDetectorParser parser = DeviceDetectorParser.getClient();
+            DeviceDetectorResult result = parser.parse(userAgent);
 
-            //ip address
-            String ipAdd = request.getRemoteAddr();
-            System.out.println("cHECKINGGGGGGG ipAdd:::::"+ipAdd);//127.0.0.1
-
-            int remotePort = request.getRemotePort();
-            System.out.println("cHECKINGGGGGGG remotePort:::::"+remotePort);//52313
-
-
-
-            // System.out.println("cHECKINGGGGGGG serviceScheme :::::"+ serviceScheme);//http
-            // System.out.println("cHECKINGGGGGGG servicePort :::::"+ servicePort);//4200
-
-            //===========================
-            // testing device detector solution A: tak jadi
-            //=========================
-
-            // DeviceDetectorParser parser = DeviceDetectorParser.getClient();
-            // DeviceDetectorResult result = parser.parse("Mozilla/5.0 (Linux; Android 7.1.1; G8232 Build/41.2.A.0.235) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36");
-
-            // System.out.println("Result found: " + result.found());
-            // System.out.println("User-agent is mobile: " + result.isMobileDevice());
-            // System.out.println("User-agent is bot: " + result.isBot());
-            // System.out.println("Result as JSON: " + result.toJSON());
+            System.out.println("Result found: " + result.found());
+            System.out.println("User-agent is mobile: " + result.isMobileDevice());
+            System.out.println("User-agent is bot: " + result.isBot());
+            System.out.println("Result as JSON: " + result.toJSON());
 
             //===============================
             RestTemplate restTemplate = new RestTemplate();
@@ -92,13 +70,13 @@ public class ProxyController {
             // =======================
 
             // See https://raw.githubusercontent.com/monperrus/crawler-user-agents/master/crawler-user-agents.json
-            URL resource = getClass().getClassLoader().getResource("static/crawler-user-agents.json");
-            //JSON parser object to parse read file
-            JSONParser jsonParser = new JSONParser();
-            FileReader reader = new FileReader(Paths.get(resource.toURI()).toFile());
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
-            JSONArray crawlerList = (JSONArray) obj;
+            // URL resource = getClass().getClassLoader().getResource("static/crawler-user-agents.json");
+            // //JSON parser object to parse read file
+            // JSONParser jsonParser = new JSONParser();
+            // FileReader reader = new FileReader(Paths.get(resource.toURI()).toFile());
+            // //Read JSON file
+            // Object obj = jsonParser.parse(reader);
+            // JSONArray crawlerList = (JSONArray) obj;
             // System.out.println(crawlerList);
             
             // =======================
