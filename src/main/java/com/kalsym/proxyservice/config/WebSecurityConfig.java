3670e7c3ed8fc39a14dab7e37570405195f316f2
nur.iman <nur.iman@kalsym.com>
2022-06-17 11:33:52 +0800

    remove filter

--- src/main/java/com/kalsym/proxyservice/config/WebSecurityConfig.java
+++ src/main/java/com/kalsym/proxyservice/config/WebSecurityConfig.java
@@ -89,7 +89,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                     .cors().and().csrf().disable();;
-            httpSecurity.addFilterBefore(sessionRequestFilter, UsernamePasswordAuthenticationFilter.class);
+            // httpSecurity.addFilterBefore(sessionRequestFilter, UsernamePasswordAuthenticationFilter.class);
         }
 
         @Bean
