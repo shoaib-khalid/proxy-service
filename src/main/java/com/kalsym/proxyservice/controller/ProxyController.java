29f31e6711bb0a33c3648adb133fec27f7b8cd14
nur.iman <nur.iman@kalsym.com>
2022-06-17 16:52:52 +0800

    print error

--- src/main/java/com/kalsym/proxyservice/controller/ProxyController.java
+++ src/main/java/com/kalsym/proxyservice/controller/ProxyController.java
@@ -254,11 +254,12 @@ public class ProxyController {
 
         } catch (Exception error) {
             System.out.println("An error has occured : " + error.getMessage());
-            // error.printStackTrace();
+            //error.printStackTrace();
             String location = "ProxyController";
 
-            LogUtil.warn("", location, "Response with  Error" , error.getMessage());
+            // LogUtil.warn("", location, "Response with  Error" , error.getMessage());
 
+            LogUtil.error("", location, "Response with  Error" ,location, error);
 
             String content = 
             "<!DOCTYPE html>"
